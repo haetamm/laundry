12c5586d7618a47911f009e6521b038610c944c7
Thaetami <tatanghaetami.97@gmail.com>
2025-10-25 09:58:09 +0700

    update

--- src/component/auth/CardNotFound.jsx
+++ src/component/auth/CardNotFound.jsx
@@ -4,7 +4,7 @@ import { FaExclamationCircle } from 'react-icons/fa'
 
 const CardNotFound = ({ title }) => {
   return (
-    <div className="flex flex-col items-center justify-center py-12">
+    <div className="xs:hidden flex flex-col items-center justify-center py-12">
         <FaExclamationCircle className="text-gray-400 text-6xl mb-4" />
         <h3 className="text-lg font-medium text-gray-900 mb-2">No {title} Found</h3>
         <p className="text-gray-500 text-center max-w-md">
